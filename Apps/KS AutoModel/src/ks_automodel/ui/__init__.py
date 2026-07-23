09f5232b522609f2af821de0f7cddd54f722c222
Moinuddin Ahmed (Jony) <gamingbir@gmail.com>
2026-07-23 01:50:26 -0400

    Delete Apps/KS AutoModel directory

--- Apps/KS AutoModel/src/ks_automodel/ui/__init__.py
+++
@@ -1,6 +0,0 @@
-"""UI components for KS AutoModel."""
-
-from .desktop.app import run
-from .desktop.main_window import MainWindow, create_main_window
-
-__all__ = ["MainWindow", "create_main_window", "run"]
